6c19fbb86171444add93f043ce24f8a407b7d941
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-19 22:19:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,10 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if min_val == nil || value < min_val
-      min_val = value
+      min_val = value      
       min_key = key
     end
     end
    min_key
 end
+ {ace: 2, bace: 3, cace: 5}
